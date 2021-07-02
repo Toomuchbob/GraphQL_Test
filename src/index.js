40acb4be75df26d6f381a152beda69c123d808c7
Gino Zaccardelli <ginozacc@gmail.com>
2021-07-01 22:40:06 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { ApolloServer } = require('apollo-server');
+
+let people = [{
+    name: 'Areg',
+    age: 35,
+    nameOfFriends: ['John']
+},
+{
+    name: 'John',
+    age: 25,
+    nameOfFriends: ['Areg']
+}]
+
+const resolvers = {
+    Query: {
+        getInfo: () => people,
+    },
+
+    Mutation: {
+        post: (p, args) => {
+            const person = {
+                name: args.name,
+                age: args.age,
+                nameOfFriends: args.nameOfFriends
+            }
+            people.push(person);
+            return person;
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+    resolvers
+})
+
+server.listen()
+    .then(({ url }) => console.log(`Server is running on ${url}`));
